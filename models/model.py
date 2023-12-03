17d5f676082be9184c8186d013958b15e243b2de
ando <terbu102@gmail.com>
2023-12-03 10:27:33 +0300

    final

--- models/model.py
+++ models/model.py
@@ -32,14 +32,5 @@ class ModelBase:
         """String representation of the BaseModel class"""
         return "[{:s}]  {}".format(self.__class__.__name__,
                                          self.__dict__)
-    def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__
-        of the instance"""
-        new_dict = self.__dict__.copy()
-        new_dict["__class__"] = self.__class__.__name__
-        new_dict["Time"] = self.Time.strftime("%Y-%m-%d %H:%M:%S")
-        if "_sa_instance_state" in new_dict.keys():
-            del new_dict["_sa_instance_state"]
-        return new_dict
     
         
